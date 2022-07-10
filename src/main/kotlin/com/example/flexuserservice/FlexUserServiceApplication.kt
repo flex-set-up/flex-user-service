0ed931a86dac32ff6f620c96eb397d61622a29d5
lyutvs <lyutvs@gmail.com>
2022-07-10 23:31:22 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/flexuserservice/FlexUserServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.flexuserservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlexUserServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<FlexUserServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/flexuserservice/FlexUserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.flexuserservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlexUserServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
